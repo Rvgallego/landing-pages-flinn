777694a02e9705302700433384a875a6ff8f9e79
Rgallego <rvgallego@hotmail.com>
2017-11-16 23:28:15 +0100

    fit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,5 @@
 class HomeController < ApplicationController
-
-  def index
-  end
-
-  def checkr
-  end
-
   def flynn
+    #
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-
-	root 'home#index'
-  get 'home/checkr', to: 'home#checkr', as: 'checkr'
-  get 'home/flynn', to: 'home#flynn', as: 'flynn'
-
+	root 'home#flynn'
 end
